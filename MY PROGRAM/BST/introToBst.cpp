57a808f63328627f977db50246d4afcbb4d101e3
anurag <anuragbadwahe@gmail.com>
2024-02-10 19:48:43 +0530

    Added Binary Search Tree

--- MY PROGRAM/BST/introToBst.cpp
+++ MY PROGRAM/BST/introToBst.cpp
@@ -1,86 +1,98 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
-class Node{
- public:
- int data;
- Node* left;
- Node* right;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
 
- //Constructor
- Node(int d){
-    this->data = d;
-    this->left = NULL;
-    this->right = NULL;
-   }
+    // Constructor
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
 };
-void levelOrderTraversal(Node* root) {
-    queue<Node*> q;
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
     q.push(root);
     q.push(NULL);
 
-    while(!q.empty()) {
-        Node* temp = q.front();
+    while (!q.empty())
+    {
+        Node *temp = q.front();
         q.pop();
 
-        if(temp == NULL) { 
-            //purana level complete traverse ho chuka hai
+        if (temp == NULL)
+        {
+            // purana level complete traverse ho chuka hai
             cout << endl;
-            if(!q.empty()) { 
-                //queue still has some child nodes
+            if (!q.empty())
+            {
+                // queue still has some child nodes
                 q.push(NULL);
-            }  
+            }
         }
-        else{
-            cout << temp -> data << " ";
-            if(temp ->left) {
-                q.push(temp ->left);
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
             }
 
-            if(temp ->right) {
-                q.push(temp ->right);
+            if (temp->right)
+            {
+                q.push(temp->right);
             }
         }
     }
-
 }
 
-Node* insertIntoBst(Node* root,int d){
-    if(root == NULL){
-       root =  new Node(d);
+Node *insertIntoBst(Node *root, int d)
+{
+    if (root == NULL)
+    {// base case
+        root = new Node(d);
         return root;
     }
 
-    if(d > root->data){
-        root->right = insertIntoBst(root->right,d);
+    if (d > root->data)
+    {
+        root->right = insertIntoBst(root->right, d);
     }
-    else{
-        root->left = insertIntoBst(root->left,d);
+    else
+    {
+        root->left = insertIntoBst(root->left, d);
     }
     return root;
 }
 
-void takeInput(Node* &root){
-    
+void takeInput(Node *&root)
+{
+
     int data;
     cin >> data;
 
-    while(data != -1){
-       root = insertIntoBst(root,data);
+    while (data != -1)
+    {
+        root = insertIntoBst(root, data);
         cin >> data;
     }
-
 }
 
+int main()
+{
+    Node *root = NULL;
+    cout << " Enter data to create BST" << endl;
+    takeInput(root);
 
+    cout << "PRINTING THE DATA" << endl;
+    levelOrderTraversal(root);
 
-int main(){
-Node* root = NULL;
-cout << " Enter data to create BST" << endl;
-takeInput(root);
-
-cout << "PRINTING THE DATA"<<endl;
-levelOrderTraversal(root);
-
-  return 0;
+    return 0;
 }
